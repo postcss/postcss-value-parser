d435e611a441b8a4b4e21cd427a03df80f07e93e
Bogdan Chadkin <opensource@trysound.io>
2026-06-05 14:23:24 +0300

    chore: replace tape with node --test (#97)
    Newer versions of node got builtin solution for testing which fits quite
    well to this project.
    
    Lockfile 2000 -> 800 lines

--- test/index.js
+++ test/index.js
@@ -1,84 +1,78 @@
-var test = require("tape");
-var parser = require("..");
+const { describe, test } = require("node:test");
+const { equal, deepEqual } = require("node:assert");
+const parser = require("..");
 
-test("ValueParser", function(tp) {
-  tp.test("i/o", function(t) {
-    var tests = [
+describe("ValueParser", () => {
+  test("i/o", () => {
+    const tests = [
       " rgba( 34 , 45 , 54, .5 ) ",
       "w1 w2 w6 \n f(4) ( ) () \t \"s't\" 'st\\\"2'"
     ];
-    t.plan(tests.length);
 
-    tests.forEach(function(item) {
-      t.equal(
+    tests.forEach(item => {
+      equal(
         item,
         parser(item)
-          .walk(function() {})
+          .walk(() => { })
           .toString(),
         JSON.stringify(item)
       );
     });
   });
 
-  tp.test("walk", function(t) {
-    t.plan(4);
-    var result;
-
-    result = [];
-
-    parser("fn( ) fn2( fn3())").walk(function(node) {
+  test("walk should process all functions", () => {
+    const result = [];
+    parser("fn( ) fn2( fn3())").walk(node => {
       if (node.type === "function") {
         result.push(node);
       }
     });
 
-    t.deepEqual(
-      result,
-      [
-        {
-          type: "function",
-          sourceIndex: 0,
-          sourceEndIndex: 5,
-          value: "fn",
-          before: " ",
-          after: "",
-          nodes: []
-        },
-        {
-          type: "function",
-          sourceIndex: 6,
-          sourceEndIndex: 17,
-          value: "fn2",
-          before: " ",
-          after: "",
-          nodes: [
-            {
-              type: "function",
-              sourceIndex: 11,
-              sourceEndIndex: 16,
-              value: "fn3",
-              before: "",
-              after: "",
-              nodes: []
-            }
-          ]
-        },
-        {
-          type: "function",
-          sourceIndex: 11,
-          sourceEndIndex: 16,
-          value: "fn3",
-          before: "",
-          after: "",
-          nodes: []
-        }
-      ],
-      "should process all functions"
-    );
+    deepEqual(result, [
+      {
+        type: "function",
+        sourceIndex: 0,
+        sourceEndIndex: 5,
+        value: "fn",
+        before: " ",
+        after: "",
+        nodes: []
+      },
+      {
+        type: "function",
+        sourceIndex: 6,
+        sourceEndIndex: 17,
+        value: "fn2",
+        before: " ",
+        after: "",
+        nodes: [
+          {
+            type: "function",
+            sourceIndex: 11,
+            sourceEndIndex: 16,
+            value: "fn3",
+            before: "",
+            after: "",
+            nodes: []
+          }
+        ]
+      },
+      {
+        type: "function",
+        sourceIndex: 11,
+        sourceEndIndex: 16,
+        value: "fn3",
+        before: "",
+        after: "",
+        nodes: []
+      }
+    ]);
+  });
 
-    result = [];
+  test("walk shouldn't process functions after falsy callback", () => {
+    const result = [];
 
-    parser("fn( ) fn2( fn3())").walk(function(node) {
+    parser("fn( ) fn2( fn3())").walk(node => {
       if (node.type === "function") {
         result.push(node);
         if (node.value === "fn2") {
@@ -88,127 +82,119 @@ test("ValueParser", function(tp) {
       return true;
     });
 
-    t.deepEqual(
-      result,
-      [
-        {
-          type: "function",
-          sourceIndex: 0,
-          sourceEndIndex: 5,
-          value: "fn",
-          before: " ",
-          after: "",
-          nodes: []
-        },
-        {
-          type: "function",
-          sourceIndex: 6,
-          sourceEndIndex: 17,
-          value: "fn2",
-          before: " ",
-          after: "",
-          nodes: [
-            {
-              type: "function",
-              sourceIndex: 11,
-              sourceEndIndex: 16,
-              value: "fn3",
-              before: "",
-              after: "",
-              nodes: []
-            }
-          ]
-        }
-      ],
-      "shouldn't process functions after falsy callback"
-    );
+    deepEqual(result, [
+      {
+        type: "function",
+        sourceIndex: 0,
+        sourceEndIndex: 5,
+        value: "fn",
+        before: " ",
+        after: "",
+        nodes: []
+      },
+      {
+        type: "function",
+        sourceIndex: 6,
+        sourceEndIndex: 17,
+        value: "fn2",
+        before: " ",
+        after: "",
+        nodes: [
+          {
+            type: "function",
+            sourceIndex: 11,
+            sourceEndIndex: 16,
+            value: "fn3",
+            before: "",
+            after: "",
+            nodes: []
+          }
+        ]
+      }
+    ]);
+  });
 
-    result = [];
+  test("walk shouldn't process nodes with defined non-function type", () => {
+    const result = [];
 
-    parser("fn( ) fn2( fn3())").walk(function(node) {
+    parser("fn( ) fn2( fn3())").walk(node => {
       if (node.type === "function" && node.value === "fn2") {
         node.type = "word";
       }
       result.push(node);
     });
 
-    t.deepEqual(
-      result,
-      [
-        {
-          type: "function",
-          sourceIndex: 0,
-          sourceEndIndex: 5,
-          value: "fn",
-          before: " ",
-          after: "",
-          nodes: []
-        },
-        { type: "space", sourceIndex: 5, sourceEndIndex: 6, value: " " },
-        {
-          type: "word",
-          sourceIndex: 6,
-          sourceEndIndex: 17,
-          value: "fn2",
-          before: " ",
-          after: "",
-          nodes: [
-            {
-              type: "function",
-              sourceIndex: 11,
-              sourceEndIndex: 16,
-              value: "fn3",
-              before: "",
-              after: "",
-              nodes: []
-            }
-          ]
-        }
-      ],
-      "shouldn't process nodes with defined non-function type"
-    );
+    deepEqual(result, [
+      {
+        type: "function",
+        sourceIndex: 0,
+        sourceEndIndex: 5,
+        value: "fn",
+        before: " ",
+        after: "",
+        nodes: []
+      },
+      { type: "space", sourceIndex: 5, sourceEndIndex: 6, value: " " },
+      {
+        type: "word",
+        sourceIndex: 6,
+        sourceEndIndex: 17,
+        value: "fn2",
+        before: " ",
+        after: "",
+        nodes: [
+          {
+            type: "function",
+            sourceIndex: 11,
+            sourceEndIndex: 16,
+            value: "fn3",
+            before: "",
+            after: "",
+            nodes: []
+          }
+        ]
+      }
+    ]);
+  });
 
-    result = [];
+  test("walk should process all functions with reverse mode", () => {
+    const result = [];
 
-    parser("fn2( fn3())").walk(function(node) {
+    parser("fn2( fn3())").walk(node => {
       if (node.type === "function") {
         result.push(node);
       }
     }, true);
 
-    t.deepEqual(
-      result,
-      [
-        {
-          type: "function",
-          sourceIndex: 5,
-          sourceEndIndex: 10,
-          value: "fn3",
-          before: "",
-          after: "",
-          nodes: []
-        },
-        {
-          type: "function",
-          sourceIndex: 0,
-          sourceEndIndex: 11,
-          value: "fn2",
-          before: " ",
-          after: "",
-          nodes: [
-            {
-              type: "function",
-              sourceIndex: 5,
-              sourceEndIndex: 10,
-              value: "fn3",
-              before: "",
-              after: "",
-              nodes: []
-            }
-          ]
-        }
-      ],
-      "should process all functions with reverse mode"
-    );
+    deepEqual(result, [
+      {
+        type: "function",
+        sourceIndex: 5,
+        sourceEndIndex: 10,
+        value: "fn3",
+        before: "",
+        after: "",
+        nodes: []
+      },
+      {
+        type: "function",
+        sourceIndex: 0,
+        sourceEndIndex: 11,
+        value: "fn2",
+        before: " ",
+        after: "",
+        nodes: [
+          {
+            type: "function",
+            sourceIndex: 5,
+            sourceEndIndex: 10,
+            value: "fn3",
+            before: "",
+            after: "",
+            nodes: []
+          }
+        ]
+      }
+    ]);
   });
 });
